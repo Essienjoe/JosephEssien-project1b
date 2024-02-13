58e8719381bdcf90e86ca61a116a71df159b5146
Essienjoe <139288275+Essienjoe@users.noreply.github.com>
2024-02-13 13:41:34 +0100

    Add files via upload
    files for the project

---
+++ script.js
@@ -0,0 +1,9 @@
+$(window).on("load", function () {
+  $(".flexslider").flexslider({
+    animation: "slide",
+    slideshoeSpeed: 2000,
+    direction: "vertical",
+    reverse: true,
+    pauseOnHover: true,
+  });
+});
